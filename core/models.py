006c3e974730fa7777b7dd0e49be161dce3e834b
Mario S J <mario2013sj@gmail.com>
2022-10-11 16:55:36 -0400

    del and update

--- core/models.py
+++ core/models.py
@@ -17,4 +17,7 @@ class Evento(models.Model):
 
     def get_data_evento(self):
         return self.data_evento.strftime("%d/%m/%y %H:%M Hrs")
+    
+    def get_data_input_evento(self):
+        return self.data_evento.strftime("%Y-%m-%dT%H:%M")
 
